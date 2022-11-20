54cd2c98735fd8b14998611f3bba62ed37e22f20
kbdancer <non3gov@gmail.com>
2022-11-20 16:38:34 +0800

    fix: reset twitter and medium link

--- src/pages/components/Footer.vue
+++ src/pages/components/Footer.vue
@@ -4,9 +4,9 @@
       <h1>FOLLOW US</h1>
       <div>
         <a href="https://discord.com/invite/RnShbvKjU4" target="_blank"><img src="@/assets/img/p1.png" alt=""></a>
-        <a href="https://medium.com/@opensui" target="_blank"><img src="@/assets/img/p2.png" alt=""></a>
+        <a href="https://medium.com/@januswallet" target="_blank"><img src="@/assets/img/p2.png" alt=""></a>
         <a href="https://github.com/seerxlabs/janus-wallet-website" target="_blank"><img src="@/assets/img/p3.png" alt=""></a>
-        <a href="https://twitter.com/opensui" target="_blank"><img src="@/assets/img/p4.png" alt=""></a>
+        <a href="https://twitter.com/JanusWallet" target="_blank"><img src="@/assets/img/p4.png" alt=""></a>
       </div>
     </div>
     <div class="links">
@@ -21,9 +21,9 @@
         </ul>
         <ul>
           <li><b>Community</b></li>
-          <li><a href="https://twitter.com/opensui" target="_blank">Twitter</a></li>
+          <li><a href="https://twitter.com/JanusWallet" target="_blank">Twitter</a></li>
           <li><a href="https://discord.com/invite/RnShbvKjU4" target="_blank">Discord</a></li>
-          <li><a href="https://medium.com/@opensui" target="_blank">Medium</a></li>
+          <li><a href="https://medium.com/@januswallet" target="_blank">Medium</a></li>
         </ul>
       </div>
     </div>
